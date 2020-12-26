57af8fb6b51ccd89352c45325627dc05d3fbf3fb
bravacoreana <bravacoreana@gmail.com>
2020-12-26 20:26:28 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: ["airbnb-base", "plugin:prettier/recommended"],
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: "module",
-    },
-    rules: {
-      "prettier/prettier": "error",
-      "no-console": "off",
-      "import/prefer-default-export": "off",
-    },
-  };
-  
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": "off",
+    "import/prefer-default-export": "off",
+  },
+  ignorePatterns: ["node_modules/"],
+};
