4d50645754876097c4e754e36df2a59adeca8294
bravacoreana <bravacoreana@gmail.com>
2021-01-05 22:22:47 +0100

    Update dateCalc.js

--- assets/js/dateCalc.js
+++ assets/js/dateCalc.js
@@ -2,11 +2,13 @@
 const createdDate = document.getElementById("createdDate-js");
 
 const dateSplit = () => {
-  const date = createdDate.innerText.split(" ", 4);
-  const month = date[1];
-  const day = date[2];
-  const year = date[3];
-  const fullDate = `${month} ${day} ${year}`;
-  createdDate.innerText = fullDate;
+  if (createdDate) {
+    const date = createdDate.innerText.split(" ", 4);
+    const month = date[1];
+    const day = date[2];
+    const year = date[3];
+    const fullDate = `${month} ${day} ${year}`;
+    createdDate.innerText = fullDate;
+  }
 };
 dateSplit();
