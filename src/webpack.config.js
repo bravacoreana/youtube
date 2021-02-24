cefd2e7126f14c1ce2c4596df509456c3972f82e
bravacoreana <bravacoreana@gmail.com>
2021-02-24 11:00:49 +0100

    Webpack

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -62,7 +62,7 @@ const config = {
       filename: "styles.css",
     }),
     new Dotenv({
-      path: path.resolve(__dirname, "../.env"),
+      path: path.resolve(__dirname, "./.env"),
     }),
     // new Dotenv(),
   ],
