fb8a28d41da6b49586494c549f143bc9b5f77881
bravacoreana <bravacoreana@gmail.com>
2021-02-25 12:21:31 +0100

    fetch api

--- src/assets/js/countryCode.js
+++ src/assets/js/countryCode.js
@@ -7,10 +7,15 @@
 //     document.getElementById("countryCode-js").innerHTML = countryCode;
 //   });
 
-const API_URL = "http://ip-api.com/json/";
+// const API_URL = "http://ip-api.com/json/";
+const API_URL = "https://ipapi.co/json/";
 
 fetch(API_URL)
-  .then((response) => response.json())
+  .then(
+    // (response) => response.json()
+    (response) => response.json()
+  )
   .then((json) => {
-    document.getElementById("countryCode-js").innerHTML = json.countryCode;
+    // console.log(json.country);
+    document.getElementById("countryCode-js").innerHTML = json.country;
   });
